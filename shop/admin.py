5a400bcd7a5dfbd9a7f88a243b5fdabb8cdf436f
Jeremy <jeremy@tab2.com>
2015-02-17 10:13:59 +0000

    Customer admin

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from shop.models import Customer, Stamp, Voucher
+
+
 # Register your models here.
+class StampInline(admin.TabularInline):
+    model = Stamp
+    extra = 1
+
+class VoucherInline(admin.TabularInline):
+    model = Voucher
+    extra = 1
+
+class CustomerAdmin(admin.ModelAdmin):
+    inlines = [StampInline, VoucherInline]
+    list_display = ('first_name', 'last_name', 'balance', 'unredeemed_vouchers')
+    list_filter = ['last_name', 'first_name']
+    search_fields = ['last_name', 'first_name']
+
+admin.site.register(Customer, CustomerAdmin)
